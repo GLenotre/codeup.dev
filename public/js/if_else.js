859ae40f280bd09d7b3485f15eeb62de5e8877b1
Gaston Lenotre <gastonlenotre@gmail.com>
2016-02-01 09:49:03 -0600

    added a while loop

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -3,8 +3,23 @@
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var color = colors[Math.floor(Math.random()*colors.length)];
 
-var favorite = 'red'; // TODO: change this to your favorite color from the list
+var favorite = 'indigo'; // TODO: change this to your favorite color from the list
 
+if (color == 'red') {
+	message = 'cherries are red'
+} else if  (color == 'orange') {
+	message = 'oranges are orange'
+} else if  (color == 'yellow') {
+	message = 'peaches are yellow'
+} else if  (color == 'green') {
+	message = 'kiwis are green'
+} else if  (color == 'blue') {
+	message = 'blueberries are blue'
+} else {
+	message = 'i don\'t know anything about that color';
+}
+console.log(color);
+console.log(message);
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
