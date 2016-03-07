637511272fbe9776515356d850ab3b9a8b42ecb8
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-07 15:15:29 -0600

    used isset function and the  superglobal variable to increment/decrement a variable

---
+++ public/counter.php
@@ -0,0 +1,51 @@
+<?php
+
+    // $_GET is an array, which is filled after form/query string use.
+    function pageController()
+    {
+    	$counterFile = 'txt/counter.txt';
+    	$handle = fopen($counterFile, 'r');
+
+    	$counter = fread($handle, filesize($counterFile));
+    	fclose($handle);
+
+    	if (isset($_GET["Down"])) {
+    		$counter--;
+    	}
+
+    	if (isset($_GET["Up"])) {
+    		$counter++;
+    	}
+
+    	$handle = fopen($counterFile, 'w');
+    	fwrite($handle, $counter);
+    	fclose($handle);
+
+        $values = array( // several values
+            'counter' => $counter,  // asssociation key/value
+        );   
+
+
+     return $values;
+
+    }
+    //var_dump($result);
+    extract(pageController());
+    //var_dump($number);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Counter PHP</title>
+    </head>
+
+    <body>
+        <div class="main">
+            <h3><?= $counter ?></h3>
+            <form><button name="Up">Up</button><button name="Down">Down</button></form>
+        </div>
+    </body>
+</html>
