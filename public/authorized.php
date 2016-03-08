310b4e232210089defa2a9889cae2cb960c4c8e1
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-08 14:07:21 -0600

    used the clearSession() function to perform a logout and redirec the user to the login

--- public/authorized.php
+++ public/authorized.php
@@ -2,13 +2,17 @@
     $items = array('Item One', 'Item Two', 'Item Three');
     $allItems = array_merge($items, $_POST);
 
+
     session_start();
 
+    // variable to hold the user name if they were logged in
     $username = ( isset($_SESSION['LOGGED_IN_USER'])) ? $_SESSION['LOGGED_IN_USER'] : "";
 
-if (isset($_SESSION['LOGGED_IN_USER'])) || $_SESSION['LOGGED_IN_USER'] == '') {
-    header('Location: /login.php');
-    die();
+    // checking the session for the 'LOGGED_IN_USER' key and making sure it has a value
+    // doing this to find out if they are logged in
+    if (isset($_SESSION['LOGGED_IN_USER'])) || $_SESSION['LOGGED_IN_USER'] == '') {
+        header('Location: /login.php');
+        die();
 ?>
 
 <!DOCTYPE html>
--- public/login.php
+++ public/login.php
@@ -1,22 +1,40 @@
 <?php
-$name = 'guest';
-$password = 'password';
 
-$correctName = isset($_POST['User']) ? $_POST['User'] : '';
-$correctPassword = isset($_POST['Password'])? $_POST['Password'] :'';
+	// starts a session or finds a session
+    // allows us to use a $_SESSION superglobal
+	session_start();
 
-if (isset($_SESSION['LOGGED_IN_USER'])) && $_SESSION['LOGGED_IN_USER'] != '') {
-	header('Location: /authorized.php');
-	die();
-}
+	//variables for login check
+	$name = 'guest';
+	$password = 'password';
 
-else if ($correctName == $name && $correctPassword == $password) {
-	$_SESSION['LOGGED_IN_USER'] = $username;
-	header('Location: /authorized.php');
-	die;
-} else if ($correctName != $name && $correctPassword != $password) {
-	echo ("Password was incorrect");
-}
+	// checking $_POST superglobal for user input to check for login
+	$correctName = isset($_POST['User']) ? $_POST['User'] : '';
+	$correctPassword = isset($_POST['Password'])? $_POST['Password'] :'';
+
+	// checking if 'LOGGED_IN_USER' key isset
+	//and if it has a value
+	//tells me that they are logged in and don't need to be here
+	if (isset($_SESSION['LOGGED_IN_USER']) && $_SESSION['LOGGED_IN_USER'] != '') {
+		//redirects to another page
+		header('Location: /authorized.php');
+		//stop all other code from running
+		die();
+	}
+	// checking to see if both values match the correct login information
+	//	doing this to login user if they entered correct information
+	else if ($correctName == $name && $correctPassword == $password) {
+		// create a session key
+		// save value as the username
+		$_SESSION['LOGGED_IN_USER'] = $username;
+		header('Location: /authorized.php');
+		die;
+
+	// checking to see if they entered the wrong information
+	// prevents meesage from showing when user submitted nothing or first time loading
+	} else if ($correctName != '' || $correctPassword != '') {
+		echo ("Password was incorrect");
+	}
 
 //Add some PHP code to the top of the login page that checks the POST'ed username and password. 
 //If the username is equal to "guest" and the password is equal to "password" 
