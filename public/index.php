573088478465593bfd59e50565f8f47c389996c6
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-09 09:59:00 -0600

    created an index file that includes navbar, footer, and header templates

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <title></title>
+    </head>
+
+    <body>
+    	<?php require_once 'navbar.php'; ?>
+    	<?php require_once 'header.php'; ?>
+    	<?php require_once 'footer.php'; ?>
+    </body>
+</html>
