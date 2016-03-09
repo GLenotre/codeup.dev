9eb48f4fd7e63d14f486e5e3773bde38d8bc123f
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-09 13:06:49 -0600

    added bootstrap template

--- public/index.php
+++ public/index.php
@@ -4,12 +4,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <title></title>
     </head>
 
     <body>
-    	<?php require_once 'navbar.php'; ?>
-    	<?php require_once 'header.php'; ?>
-    	<?php require_once 'footer.php'; ?>
+
+    	<?php include 'navbar.php'; ?>
+    	<?php include 'header.php'; ?>
+    	<?php include 'footer.php'; ?>
     </body>
 </html>
