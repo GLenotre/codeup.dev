c2943a73acb43ae5c8c398779f3d2d15e00c00d9
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-10 11:50:09 -0600

    added constructor and destructor functions

--- public/log.php
+++ public/log.php
@@ -1,9 +1,18 @@
 <?php
+
 class Log
 
 { 
 	public $filename;  // property, variable visible only inside the class Log,
 	// can be used in any method with the keyword $this->
+	public $handle;
+
+    public function __construct($prefix = 'log')  // 'log' will the default parameter
+{
+    $date = date("Y-m-d");
+    $this->filename=$prefix . $date . '.log';
+    $this->handle = fopen($this->filename, 'a')
+}
 
 public function logMessage($logLevel, $message)  // method
 	{
@@ -14,11 +23,11 @@ public function logMessage($logLevel, $message)  // method
 
     if ($logLevel == 'info') {
     	return $this->logInfo($message);
-    } elseif ($logLevel == 'error') {
+    } else ($logLevel == 'error') {
     	return $this->logError($message);
-    } else {
-    	return "[UNK], '{$logLevel}' is unknown.";
-    }
+    } 
+
+    $this->handle 
 	}
 
 // The logInfo and logError functions create different messages to be printed out by the logMessages functions. 
@@ -33,13 +42,17 @@ public function logError($message)
 	}
 
 // This function creates or opens the file
-// The fwrite line adds the message, $formatedDateTimeEntry to the new or opened file.
+// The fwrite line adds the message, 
 public function writeFile($message) 
 {
 	$handle = fopen($this->filename, 'a');
 	fwrite($handle, PHP_EOL . $message);
-	fclose($handle);	
 }
+
+public function __destruct()
+    {
+     fclose($handle);
+    }
 
 }
 
