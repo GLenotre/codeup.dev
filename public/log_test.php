fea404d302b1be3141c5fd30d6baa84f214c5d18
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-10 14:00:44 -0600

    used static methods Input::has() and Input::get() for ping and pong

--- public/log_test.php
+++ public/log_test.php
@@ -3,7 +3,7 @@
 require_once('log.php');
 
 $logger = new Log();   // $logger is an object (instance) of the class log
-$logger->logMessage("info", "Today is"); // the object $logger is calling a  method
+$logger->logMessage("info", "Today is"); // the object $logger is calling a  method, which is defined in the class Log
 $logger->logMessage("error", "Try again");
 
 ?>
