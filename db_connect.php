43817687306bfb755f7ee4fb663ab3ba1ae81d12
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-17 14:06:12 -0500

    used constants to define

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST','127.0.0.1');
+define('DB_NAME','employees');
+define('DB_USER','vagrant');
+define('DB_PASS','vagrant');
+
+$dbc = new PDO
+	(
+	'mysql:host=' . DB_HOST . ';dbname' . DB_NAME,
+	DB_USER,
+	DB_PASS
+	);
+
+$dbc ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
---
+++ pdo_test.php
@@ -0,0 +1,5 @@
+<?php
+
+REQUIRE 'db_connect.php';
+
+echo $dbc ->getAttribute(PDO::ATTR_CONNECTION_STATUS);
