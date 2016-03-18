66b6c8132928a9da92b061a4458dd1529d9ad874
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-18 13:34:12 -0500

    first commit

---
+++ national_parks.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once '../db_connect.php';
+require_once '../php/parks_login.php';
+
+If (Input::has('park_id')) {
+	$id = Input::get('park_id');
+	$stmt = $dbc->query("SELECT * FROM parks where id = $id");
+}
+
+?>
+
+<html lang="en">
+<head>
+    <title>National Parks</title>
+
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/national_parks.css">
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/js/lib/bootstrap.min.js"></script>
+</head>
+<body>
+	<table class="table">
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Location</td>
+                <td>Date Established</td>
+                <td>Area in Acres</td>
+                <td>Description</td>
+            </tr>
+        </thead>
+	</table>
+</body>
+</html>
+
+
